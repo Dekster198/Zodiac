f2f92896face04755656b44fa6767e28a80b6462
Oleg <oleg.fisenko2013@yandex.ru>
2022-08-22 04:00:54 +0300

    added a small interface blank

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+import sys
 import requests
 from bs4 import BeautifulSoup
 import fake_useragent
+from PyQt5 import QtWidgets
+from GUI import Ui_MainWindow
+
+class mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+
+app = QtWidgets.QApplication([])
+application = mywindow()
+application.show()
+
+sys.exit(app.exec())
 
 user = fake_useragent.UserAgent().random
 header = {'user-agent': user}
