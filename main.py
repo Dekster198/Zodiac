1c24bc03d284db48deb0c4a45e6cc0db52bf25c9
Oleg <oleg.fisenko2013@yandex.ru>
2022-08-24 04:00:07 +0300

    some tests

--- main.py
+++ main.py
@@ -4,23 +4,24 @@ from bs4 import BeautifulSoup
 import fake_useragent
 from PyQt5 import QtWidgets
 from GUI import Ui_MainWindow
-
-class mywindow(QtWidgets.QMainWindow):
-    def __init__(self):
-        super(mywindow, self).__init__()
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
-
+from GUI import mywindow
 
 app = QtWidgets.QApplication([])
 application = mywindow()
 application.show()
 
-sys.exit(app.exec())
-
 user = fake_useragent.UserAgent().random
 header = {'user-agent': user}
 
 url = 'https://orakul.com/'
-response = requests.get(url, headers=header)
+aquarius_url = 'https://orakul.com/horoscope/astrologic/general/aries/today.html'
+response = requests.get(aquarius_url, headers=header)
 soup = BeautifulSoup(response.text, 'lxml')
+
+aquarius_block = soup.find('div', class_='contentOnly')
+birthday = aquarius_block.find('div', class_='sm-descr')
+horo_date = aquarius_block.find('h2', class_='typehead')
+horo_block = aquarius_block.find('p', class_='')
+print(f'Дата рождения: {birthday.text.strip()}\n{horo_date.text.strip()}\n{horo_block.text.strip()}')
+
+sys.exit(app.exec())
