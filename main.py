82d6628f48188c4e639f006b14ad82d8681eb60a
Oleg <oleg.fisenko2013@yandex.ru>
2022-08-25 04:32:33 +0300

    add a class of parsing

--- main.py
+++ main.py
@@ -4,24 +4,30 @@ from bs4 import BeautifulSoup
 import fake_useragent
 from PyQt5 import QtWidgets
 from GUI import Ui_MainWindow
-from GUI import mywindow
+from parser import Parser
 
-app = QtWidgets.QApplication([])
-application = mywindow()
-application.show()
+class mywindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(mywindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-user = fake_useragent.UserAgent().random
-header = {'user-agent': user}
+def main():
+    app = QtWidgets.QApplication([])
+    application = mywindow()
+    application.show()
 
-url = 'https://orakul.com/'
-aquarius_url = 'https://orakul.com/horoscope/astrologic/general/aries/today.html'
-response = requests.get(aquarius_url, headers=header)
-soup = BeautifulSoup(response.text, 'lxml')
+    signs = {'овен': 'aries', 'телец': 'taurus', 'близнецы': 'gemini', 'рак': 'cancer', 'лев': 'leo', 'дева': 'virgo',
+             'весы': 'libra', 'скорпион': 'scorpio', 'стрелец': 'sagittarius', 'козерог': 'capricorn', 'водолей': 'aquarius',
+             'рыбы': 'pisces'}
+    sign = input('Введите знак зодиака: ').lower()
+    while sign not in signs:
+        sign = input('Неправильно введён знак зодиака. Повторите попытку: ').lower()
+    horo = Parser(signs[sign])
+    horo.parsing()
+    horo.print_horo()
 
-aquarius_block = soup.find('div', class_='contentOnly')
-birthday = aquarius_block.find('div', class_='sm-descr')
-horo_date = aquarius_block.find('h2', class_='typehead')
-horo_block = aquarius_block.find('p', class_='')
-print(f'Дата рождения: {birthday.text.strip()}\n{horo_date.text.strip()}\n{horo_block.text.strip()}')
+    sys.exit(app.exec())
 
-sys.exit(app.exec())
+if __name__ == '__main__':
+    main()
---
+++ parser.py
@@ -0,0 +1,22 @@
+import requests
+from bs4 import BeautifulSoup
+import fake_useragent
+
+class Parser:
+    user = fake_useragent.UserAgent().random
+    header = {'user-agent': user}
+    def __init__(self, sign):
+        self.sign = sign
+    def parsing(self):
+        url = f'https://orakul.com/horoscope/astrologic/general/{self.sign}/today.html'
+        response = requests.get(url, headers=self.header)
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        block = soup.find('div', class_='contentOnly')
+        self.horo_title = block.find('h1')
+        self.birthday = block.find('div', class_='sm-descr')
+        self.horo_date = block.find('h2', class_='typehead')
+        self.horo_block = block.find('p', class_='')
+    def print_horo(self):
+        print(f'Знак зодиака: {self.horo_title.text.strip()}\nДата рождения: {self.birthday.text.strip()}\n{self.horo_date.text.strip()}\n'
+              f'{self.horo_block.text.strip()}')
